284de94cb87920ab60bb41060a3595fc087ac347
Michael Chan <mhcchan@ucdavis.edu>
2021-08-25 16:29:37 -0700

    More OAuth

--- server/models/v1/oauth/github.js
+++ server/models/v1/oauth/github.js
@@ -1,6 +1,42 @@
 // https://github.com/login/oauth/authorize?client_id=4d815e2740249b35e920&redirect_uri=http://localhost:40300/v1/oauth/github/handler&state=dfgafdsg54gw45gfds
-module.exports = (req, res, next) => {
+
+const axios = require('axios');
+const qs = require('querystring');
+
+const verifyWithGitHub = async (code) => {
+  try {
+    const { data: authenticationObject } = await axios({
+      url: 'https://github.com/login/oauth/access_token',
+      method: 'POST',
+      data: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code,
+      },
+    });
+    const { access_token: accessToken } = qs.parse(authenticationObject);
+    const { data: { id: gitHubUserId } } = await axios({
+      url: 'https://api.github.com/user',
+      headers: {
+        Authorization: `token ${accessToken}`,
+      },
+    });
+    return [null, gitHubUserId];
+  } catch (e) {
+    return [e];
+  }
+};
+
+const handler = async (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  console.log(req.query);
-  res.json('test');
+  const [authError, gitHubId] = await verifyWithGitHub(req.query.code);
+  if (authError) {
+    console.log(authError);
+    return next([authError]);
+  }
+  return next([200, gitHubId]);
+};
+
+module.exports = {
+  handler,
 };
--- server/routes/v1/oauth/github/handler.js
+++ server/routes/v1/oauth/github/handler.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const GitHubModel = require('#models/v1/oauth/github.js');
 
-router.get('/', GitHubModel);
+router.get('/', GitHubModel.handler);
 
 module.exports = router;
