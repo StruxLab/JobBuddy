f81963c1aca735bf91fdc93aa6440d36c41d8378
Michael Chan <mhcchan@ucdavis.edu>
2022-02-14 22:04:53 -0800

    authentication flow

--- next-app/pages/login.tsx
+++ next-app/pages/login.tsx
@@ -1,5 +1,21 @@
+import { signIn, useSession } from 'next-auth/react';
+
 export default function Login() {
+  const { data: session, status } = useSession();
   return (
-    <div>Login</div>
+    <div>
+      <div>
+        Logged in as: { session?.user?.name }
+      </div>
+      <button onClick={() => signIn('google')}>
+        Sign in with Google
+      </button>
+      <button onClick={() => signIn('linkedin')}>
+        Sign in with linkedin
+      </button>
+      <button onClick={() => signIn('github')}>
+        Sign in with github
+      </button>
+    </div>
   );
 }
