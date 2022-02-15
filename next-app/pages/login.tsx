b4d79186786c9a72da9c1526600e2ca78f16c762
Michael Chan <mhcchan@ucdavis.edu>
2022-02-14 23:17:45 -0800

    Typescripting

--- next-app/pages/login.tsx
+++ next-app/pages/login.tsx
@@ -1,11 +1,21 @@
-import { signIn, useSession } from 'next-auth/react';
+import { signIn, getSession } from 'next-auth/react';
+import type { ReactElement } from 'react';
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function Login() {
-  const { data: session, status } = useSession();
+declare type ISODateString = string;
+interface LoginProps {
+    user?: {
+        name?: string | null;
+        email?: string | null;
+        image?: string | null;
+    };
+};
+
+export default function Login({ user }: LoginProps): ReactElement {
   return (
     <div>
       <div>
-        Logged in as: { session?.user?.name }
+      Logged in as: { user?.name }
       </div>
       <button onClick={() => signIn('google')}>
         Sign in with Google
@@ -18,4 +28,23 @@ export default function Login() {
       </button>
     </div>
   );
+}
+
+interface Context {
+  req: NextApiRequest;
+  res: NextApiResponse;
+  params?: { [key: string]: any }
+};
+interface GetServerSideProps {
+  props: LoginProps;
+};
+
+export async function getServerSideProps(context: Context): Promise<GetServerSideProps> {
+  console.log(context);
+  const session = await getSession(context);
+  return {
+    props: {
+      user: session?.user,
+    },
+  };
 }
