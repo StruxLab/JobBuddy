e15122b23b280fc36f2aeaeaa9575916139b5189
Michael Chan <mhcchan@ucdavis.edu>
2022-02-14 23:23:40 -0800

    🔒 Core Next.js authentication logic (#25)
    * Microfrontend Path Adjustments
    
    * dependencies
    
    * example env
    
    * authentication flow
    
    * Typescripting
    
    * remove consolelog
    
    * remove unused type

---
+++ next-app/pages/login.tsx
@@ -0,0 +1,48 @@
+import { signIn, getSession } from 'next-auth/react';
+import type { ReactElement } from 'react';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+interface LoginProps {
+    user?: {
+        name?: string | null;
+        email?: string | null;
+        image?: string | null;
+    };
+};
+
+export default function Login({ user }: LoginProps): ReactElement {
+  return (
+    <div>
+      <div>
+      Logged in as: { user?.name }
+      </div>
+      <button onClick={() => signIn('google')}>
+        Sign in with Google
+      </button>
+      <button onClick={() => signIn('linkedin')}>
+        Sign in with linkedin
+      </button>
+      <button onClick={() => signIn('github')}>
+        Sign in with github
+      </button>
+    </div>
+  );
+}
+
+interface Context {
+  req: NextApiRequest;
+  res: NextApiResponse;
+  params?: { [key: string]: any }
+};
+interface GetServerSideProps {
+  props: LoginProps;
+};
+
+export async function getServerSideProps(context: Context): Promise<GetServerSideProps> {
+  const session = await getSession(context);
+  return {
+    props: {
+      user: session?.user,
+    },
+  };
+}
