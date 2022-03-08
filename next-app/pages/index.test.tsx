c8a2226692891b294f3439ab29ce9ddaa88be067
Michael Chan <mhcchan@ucdavis.edu>
2022-03-07 20:15:48 -0800

    📝  feat: Testing suite setup, with GitHub workflow and CodeCov (#35)
    * testing suite setup - jest + rtl
    
    * Codecoverage workflow

---
+++ next-app/pages/index.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import Home from './index';
+
+describe('test', () => {
+  it('renders a heading', () => {
+    // render(<Home />);
+
+    // const heading = screen.getByRole('heading', {
+    //   name: /welcome to next\.js!/i,
+    // });
+
+    // expect(heading).toBeInTheDocument();
+    expect(1).toEqual(1);
+  });
+});
